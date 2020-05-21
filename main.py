b10d6b2cdba5ecb950ed851b037a44430e6e1e9a
Shaheryar <shaheryartariq909@gmail.com>
2020-05-21 14:57:42 +0500

    1. Added necessary files

---
+++ main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI,File, UploadFile
+import numpy as np
+import io
+import base64
+from PIL import Image
+from caption import *
+import cv2
+import base64
+
+
+
+
+
+
+
+def encode(img):
+    pil_img = Image.fromarray(img)
+    buff = io.BytesIO()
+    pil_img.save(buff, format="JPEG")
+    new_image_string = base64.b64encode(buff.getvalue()).decode("utf-8")
+    return  new_image_string
+
+
+app = FastAPI()
+
+
+@app.post("/generate_caption")
+async def root(file: bytes = File(...)):
+    print("abc")
+    image = Image.open(io.BytesIO(file)).convert("RGB")
+    img = np.array(image)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    cv2.imwrite('a.jpg',img)
+    caption = generate_caption('a.jpg',5)
+    return {"caption":caption}
